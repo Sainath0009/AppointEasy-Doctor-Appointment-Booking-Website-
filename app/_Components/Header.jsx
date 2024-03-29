c3d9c25ba0b05b950563bc3fd0da8a43d1e16343
Sainath0009 <sainathmedpalli@.com>
2024-03-29 18:34:46 +0530

    Hero section added

--- app/_Components/Header.jsx
+++ app/_Components/Header.jsx
@@ -1,22 +1,27 @@
 import { Button } from '@/components/ui/button'
-import { Link } from 'lucide-react'
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 function Header() {
     const Menu = [
         {
-            id: 1,
+            id:1,
             name: "Home",
             path: '/'
         },
         {
-            id: 2,
+            id:2,
+            name: "Find a Doctor",
+            path: '/'
+        },
+        {
+            id:3,
             name: "Explore",
             path: '/'
         },
         {
-            id: 3,
+            id:4,
             name: "Contact Us",
             path: '/'
         },
@@ -24,18 +29,20 @@ function Header() {
     return (
         <div className='flex items-center justify-between p-4 shadow-sm'>
             <div className='flex items-center gap-10'>
-                <Image src='/logo.jpg' alt='logo'
-                    width={180} height={190} />
+                <Image src='/logo.png' alt='logo'
+                    width={200} height={150} />
 
                 <ul className='md:flex gap-8 hidden'>
                     {Menu.map((item, index) => (
-                        <Link href={item.path}>
-                        <li className='hover:text-primary cursor-pointer hover:scale-105 translate-all ease-in-out'>{item.name}</li>
-                        </Link>
+                        <li key={item.id} className='hover:text-primary cursor-pointer hover:scale-105 translate-all ease-in-out'>
+                            <Link href={item.path}>
+                                {item.name}
+                            </Link>
+                        </li>
                     ))}
                 </ul>
             </div>
-        <Button>Get Started</Button>
+            <Button>Get Started</Button>
         </div>
     )
 }
--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button"
+import Hero from "./_Components/Hero";
 
 export default function Home() {
   return (
  <div >
-  
+  {/* Hero-section */}
+  <Hero/>
  </div>
   );
 }
